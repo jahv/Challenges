a3a61d721236c16815d8424a81b546bb205e558e
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-06-22 17:52:01 -0500

    [HackerRank] 10 days of statistics. Day 1

--- src/main/java/hackerranck/tutorials/TenDaysStatistics.java
+++ src/main/java/hackerranck/tutorials/TenDaysStatistics.java
@@ -1,9 +1,17 @@
 package hackerranck.tutorials;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TenDaysStatistics {
+
+    private static final String Q1 = "Q1";
+    private static final String Q2 = "Q2";
+    private static final String Q3 = "Q3";
 
     public static double mean(int[] numbers) {
         double mean = 0.0;
@@ -54,6 +62,50 @@ public class TenDaysStatistics {
         return numbersByWeightsSum / weightsSum;
     }
 
+    public static Map<String, Double> quartiles(int numbers[]) {
+        double q2 = median(numbers);
+        int mid = numbers.length/2;
+        int halfDown[];
+        int halfUp[];
+
+        if(numbers.length % 2 == 0) {
+            halfDown = Arrays.copyOfRange(numbers, 0, mid);
+            halfUp = Arrays.copyOfRange(numbers, mid, numbers.length);
+        } else {
+            halfDown = Arrays.copyOfRange(numbers, 0, mid);
+            halfUp = Arrays.copyOfRange(numbers, mid + 1, numbers.length);
+        }
+
+        double q1 = median(halfDown);
+        double q3 = median(halfUp);
+
+        Map<String, Double> results = new HashMap<>();
+        results.put(Q1, q1);
+        results.put(Q2, q2);
+        results.put(Q3, q3);
+
+        return results;
+    }
+
+    public static double interquartileRange(int numbers[]) {
+        Map<String, Double> quartiles = quartiles(numbers);
+        double q1 = quartiles.get(Q1);
+        double q3 = quartiles.get(Q3);
+        return q3 - q1;
+    }
+
+    public static double standardDeviation(int numbers[]) {
+        double mean = mean(numbers);
+
+        double avgSquaredDistance = 0.0;
+        for(int i=0; i<numbers.length; i++) {
+            avgSquaredDistance += Math.pow(numbers[i]-mean, 2);
+        }
+
+        avgSquaredDistance = avgSquaredDistance / numbers.length;
+        return Math.sqrt(avgSquaredDistance);
+    }
+
     public static void day0_mean_median_mode() {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -61,7 +113,7 @@ public class TenDaysStatistics {
         String numbersStr = scan.nextLine();
         scan.close();
 
-        int[] numbers = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int numbers[] = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
 
         double mean = mean(numbers);
         double median = median(numbers);
@@ -80,16 +132,72 @@ public class TenDaysStatistics {
         String weightsStr = scan.nextLine();
         scan.close();
 
-        int[] numbers = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] weights = Arrays.stream(weightsStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int numbers[] = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int weights[] = Arrays.stream(weightsStr.split(" ")).mapToInt(Integer::parseInt).toArray();
 
         double meanWeight = meanWeight(numbers, weights);
 
         System.out.printf("%.1f\n", meanWeight);
     }
 
+    public static void day1_quartiles() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        String numbersStr = scan.nextLine();
+        scan.close();
+
+        int numbers[] = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Map<String, Double> quartiles = quartiles(numbers);
+
+        System.out.printf("%.0f\n", quartiles.get(Q1));
+        System.out.printf("%.0f\n", quartiles.get(Q2));
+        System.out.printf("%.0f\n", quartiles.get(Q3));
+    }
+
+    public static void day1_interquartile_range() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        String numbersStr = scan.nextLine();
+        String frequencyStr = scan.nextLine();
+        scan.close();
+
+        int numbers[] = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int frequency[] = Arrays.stream(frequencyStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> finalNumbers = new ArrayList<>();
+
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=0; j<frequency[i]; j++) {
+                finalNumbers.add(numbers[i]);
+            }
+        }
+
+        double interquartileRange = interquartileRange(finalNumbers.stream().mapToInt(i -> i).toArray());
+
+        System.out.printf("%.1f\n", interquartileRange);
+    }
+
+    public static void day1_standard_deviation() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        String numbersStr = scan.nextLine();
+        scan.close();
+
+        int numbers[] = Arrays.stream(numbersStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        double standardDeviation = standardDeviation(numbers);
+
+        System.out.printf("%.1f", standardDeviation);
+    }
+
     public static void main(String[] args) {
 //        day0_mean_median_mode();
-        day0_weightedMean();
+//        day0_weightedMean();
+//        day1_quartiles();
+//        day1_interquartile_range();
+        day1_standard_deviation();
     }
 }
